d2f060f30905c0efe2f4d262ab6cbffb2a2226d0
strauman <>
2010-01-27 19:38:26 +0000

    2010-01-27 (T.S.)
    	* src/Makefile, src/fcom_iocsh.c, src/fcomIocshSupport.dbd:
    	The latter two are new files used to implement a library
    	'libfcomIocshSupport' that registers (so far)
    	 o fcomInit()
    	 o fcomDumpStats()
    	 o fcomStrerror()
    	with iocsh().

---
+++ src/fcom_iocsh.c
@@ -0,0 +1,87 @@
+#include <iocsh.h>
+#include <registry.h>
+#include <epicsExport.h>
+#include <stdio.h>
+
+#include <fcom_api.h>
+
+static const struct iocshArg _fcomInitArgs[] = {
+	{
+	"MC prefix <ip[:port]>",
+	iocshArgString
+	},
+	{
+	"num RX buffers",
+	iocshArgInt
+	}
+};
+
+static const struct iocshArg *_fcomInitArgsp[] = {
+	&_fcomInitArgs[0],
+	&_fcomInitArgs[1],
+	0
+};
+
+struct iocshFuncDef _fcomInitDesc = {
+	"fcomInit",
+	2,
+	_fcomInitArgsp
+};
+
+static void
+_fcomInitFunc(const iocshArgBuf *args)
+{
+	fcomInit(args[0].sval, args[1].ival);
+}
+
+static const struct iocshArg *_fcomDumpStatsArgsp[] = {
+	0
+};
+
+struct iocshFuncDef _fcomDumpStatsDesc = {
+	"fcomDumpStats",
+	0,
+	_fcomDumpStatsArgsp
+};
+
+static void
+_fcomDumpStatsFunc(const iocshArgBuf *args)
+{
+	fcomDumpStats(stdout);
+}
+
+static const struct iocshArg _fcomStrerrorArgs[] = {
+	{
+	"error code",
+	iocshArgInt
+	},
+};
+
+
+static const struct iocshArg *_fcomStrerrorArgsp[] = {
+	&_fcomStrerrorArgs[0],
+	0
+};
+
+struct iocshFuncDef _fcomStrerrorDesc = {
+	"fcomStrerror",
+	1,
+	_fcomStrerrorArgsp
+};
+
+static void
+_fcomStrerrorFunc(const iocshArgBuf *args)
+{
+	fprintf(stderr,"%s\n",fcomStrerror(args[0].ival));
+}
+
+
+static void
+fcomRegistrar(void)
+{
+	iocshRegister(&_fcomInitDesc,      _fcomInitFunc);
+	iocshRegister(&_fcomDumpStatsDesc, _fcomDumpStatsFunc);
+	iocshRegister(&_fcomStrerrorDesc,  _fcomStrerrorFunc);
+}
+
+epicsExportRegistrar(fcomRegistrar);
